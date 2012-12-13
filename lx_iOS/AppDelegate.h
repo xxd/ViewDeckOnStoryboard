98615bcbfd9bba81a98f1b3cfc86d33923e24ace
xxd <xuexiaodong79@gmail.com>
2012-12-13 10:13:56 +0800

    Initial Commit

---
+++ lx_iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lx_iOS
+//
+//  Created by xxd on 12-12-13.
+//  Copyright (c) 2012年 xxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lx_iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lx_iOS
+//
+//  Created by xxd on 12-12-13.
+//  Copyright (c) 2012年 xxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
